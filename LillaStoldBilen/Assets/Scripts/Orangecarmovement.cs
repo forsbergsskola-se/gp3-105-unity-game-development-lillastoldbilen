61b841145849eaea1972d7a453428b5b3b469038
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-17 16:09:24 +0200

    Fixed Compile errors in OrangeCarMovement script

--- LillaStoldBilen/Assets/Scripts/Orangecarmovement.cs
+++ LillaStoldBilen/Assets/Scripts/Orangecarmovement.cs
@@ -5,16 +5,17 @@ public class Orangecarmovement : MonoBehaviour
     public float moveSpeed;
     public float crashForce;
     public float maxSpeed;
+    public float speedForce;
     
     // Start is called before the first frame update
     void Start()
     {
         
         transform.Translate(0, 0, moveSpeed * Time.deltaTime);
-        rigidbody rigidbody = GetComponent<Rigidbody>();
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(0, 0, speedForce);
         rigidbody.AddForce(0, 0, maxSpeed);
-        rigidbody.angularVelocity = new vector 3(0, 0, 0);
+        rigidbody.angularVelocity = new Vector3(0, 0, 0);
     }
     
 
