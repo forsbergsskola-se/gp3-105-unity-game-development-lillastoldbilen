2c7dfa68c13c040b1abdee02abddf8aac7942869
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-17 16:12:41 +0200

    Removed Void Update() from OrangeCarMovement

--- LillaStoldBilen/Assets/Scripts/Orangecarmovement.cs
+++ LillaStoldBilen/Assets/Scripts/Orangecarmovement.cs
@@ -6,31 +6,21 @@ public class Orangecarmovement : MonoBehaviour
     public float crashForce;
     public float maxSpeed;
     public float speedForce;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
         transform.Translate(0, 0, moveSpeed * Time.deltaTime);
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(0, 0, speedForce);
         rigidbody.AddForce(0, 0, maxSpeed);
         rigidbody.angularVelocity = new Vector3(0, 0, 0);
     }
+
     bool IstouchingTheGround()
     {
         int layerMask = LayerMask.GetMask("Ground");
         return Physics.CheckBox(transform.position, transform.lossyScale / 1.99f, transform.rotation, layerMask);
     }
 }
-    
-    
-    
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-
