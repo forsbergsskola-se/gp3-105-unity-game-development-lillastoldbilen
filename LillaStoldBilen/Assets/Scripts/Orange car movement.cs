42f04d76feae0a48e5d3f3c617e90392779fa3a7
RachelLach <144023790+RachelLach@users.noreply.github.com>
2023-10-17 14:48:29 +0200

    First code for orange car movement

--- LillaStoldBilen/Assets/Scripts/Orange car movement.cs
+++ LillaStoldBilen/Assets/Scripts/Orange car movement.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Orangecarmovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public float crashForce;
+    public float maxSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(0, 0, speedForce);
+        rigidbody.AddForce(0, 0, maxSpeed);
+        rigidbody.angularVelocity = new vector 3 (0,0,0)
     }
 
     // Update is called once per frame
