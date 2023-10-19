e5005a82c54513807c8d314e2e983f974ecf65e8
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-19 11:01:46 +0200

    Added a Lerp to FollowPlayer Script
    This gives a slight delay to the camera when the player is moving, giving it a smoother feel

--- LillaStoldBilen/Assets/Scripts/FollowPlayer.cs
+++ LillaStoldBilen/Assets/Scripts/FollowPlayer.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    public float smoothSpeed = 5;
     private void LateUpdate()
     {
         if (this.player == null)
@@ -13,7 +14,7 @@ public class FollowPlayer : MonoBehaviour
 
         Vector3 target = player.transform.position + player.transform.TransformDirection(new Vector3(0, 10f, -10));
         Quaternion camRotation = player.transform.rotation * Quaternion.Euler(40f, 0, 0);
-        transform.position = target;
-        transform.rotation = camRotation;
+        transform.position = Vector3.Lerp(transform.position, target, smoothSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, camRotation, smoothSpeed * Time.deltaTime);
     }
 }
