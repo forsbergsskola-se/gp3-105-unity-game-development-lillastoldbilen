1bd9ae17827f360122e5868fe688bef2ca65b791
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-19 15:00:29 +0200

    Update Bullet Prefab

--- LillaStoldBilen/Assets/Scripts/BulletBehavior.cs
+++ LillaStoldBilen/Assets/Scripts/BulletBehavior.cs
@@ -3,14 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Android;
+using UnityEngine.UIElements;
 
 public class BulletBehavior : MonoBehaviour
 {
     public float OnscreenDelay = 001f;
     public GameObject BulletFire;
-    private void OnCollisionEnter(Collision other)
+    public void OnCollisionEnter(Collision other)
     {
-        GameObject bulletFire = Instantiate(BulletFire, this.transform);
         Destroy(this.gameObject, OnscreenDelay);
+        GameObject bulletFire = Instantiate(BulletFire, this.transform);
     }
 }
