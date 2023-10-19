37733763c67019be4f26822189b3f3e66a0326a7
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-19 14:26:01 +0200

    Removed Serialization and added Mono, Created GO's quest and created Quest 0

--- LillaStoldBilen/Assets/Scripts/Quest.cs
+++ LillaStoldBilen/Assets/Scripts/Quest.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Quest
+public class Quest : MonoBehaviour
 {
     public bool isActive;
 
