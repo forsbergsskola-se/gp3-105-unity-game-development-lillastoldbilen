8a6a2f4ffee856aeb6764a8bfce4f6dfe1d49b1c
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-24 13:37:56 +0200

    Update Inventory System

--- LillaStoldBilen/Assets/Scripts/Player.cs
+++ LillaStoldBilen/Assets/Scripts/Player.cs
@@ -6,19 +6,26 @@ using UnityEngine.Events;
 public class Player : MonoBehaviour
 
 {
-    public UnityEvent Items;
     public int Health = 90;
     public float MovementSpeed;
     public GameObject onHand;
     public int MoneyBag;
-    
+    public GameObject [] inventory;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
             Action();
     }
+
     public void Action()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2.0f))
+        {
+            GameObject itemToPickUp = hit.collider.GetComponent<GameObject>();
+            inventory[0] = itemToPickUp;
+            Debug.Log($"Picked up {itemToPickUp}");
+        }
     }
-} 
+}
