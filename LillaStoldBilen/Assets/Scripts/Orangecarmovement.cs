1098fcc5b42a1bba7c80511e169a48b20d4506db
RachelLach <144023790+RachelLach@users.noreply.github.com>
2023-10-24 15:07:33 +0200

    Update Enter Car and Exit Car Logic

--- LillaStoldBilen/Assets/Scripts/Orangecarmovement.cs
+++ LillaStoldBilen/Assets/Scripts/Orangecarmovement.cs
@@ -9,7 +9,9 @@ public class Orangecarmovement : MonoBehaviour
     public bool InCar = false;
     public GameObject Player;
     public FollowPlayer followPlayer;
-
+    public PlayerController playerController;
+    
+    
     public void ActivateCar()
     {
         
@@ -46,22 +48,22 @@ public class Orangecarmovement : MonoBehaviour
 
     public void ExitCar()
     {
-        
-        //transform.position = transform.position;
-        //transform.rotation = transform.rotation;
-        
-        // here, undo everything you've done in EnterCar
-       
-        InCar = false;
+        PlayerController playerController = this.playerController;
+        this.playerController = null;
+       // get playerController from the field
+       // reset the field
+       playerController.gameObject.SetActive(true);  // here, undo everything you've done in EnterCar
+       InCar = false;
+       followPlayer.player = null;
     }
     
     public void EnterCar(PlayerController playerController)
     {
         playerController.gameObject.SetActive(false);
         InCar = true;
-
         followPlayer.player = this.gameObject;
-        // reference followPlayer script
-        // thatScript.player = this.gameObject;
+        
+        // save playerController to a field
+        this.playerController = playerController;
     } 
 }
