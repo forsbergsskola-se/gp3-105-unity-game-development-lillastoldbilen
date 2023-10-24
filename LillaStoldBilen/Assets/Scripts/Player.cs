8c97474fd91fa1e736db0cff8c4fc875aeb0d4a3
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-24 16:11:41 +0200

    Update

--- LillaStoldBilen/Assets/Scripts/Player.cs
+++ LillaStoldBilen/Assets/Scripts/Player.cs
@@ -10,21 +10,19 @@ public class Player : MonoBehaviour
     public int Health = 90;
     public float MovementSpeed;
     public int MoneyBag;
-    public Item onHand;
-    
+    public bool onHand;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (onHand)
         {
-            UseItem(onHand);
+            EquipedItem();
         }
-            
         
         
         
         
-        //For item-pick ups
-        RaycastHit hit; 
+        //For item-pick ups (Unfinished!)
+        /*RaycastHit hit; 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2.0f) == FindObjectOfType<Item>())
         
         {
@@ -32,10 +30,10 @@ public class Player : MonoBehaviour
                 Debug.Log($"Picked up {FindObjectOfType<Item>()}");
             
         }
+        */
     }
 
-    public void UseItem()
+    public void EquipedItem()
     {
-        
     }
 }
