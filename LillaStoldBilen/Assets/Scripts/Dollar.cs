b4d1f45f2f13a145c9f6b709a7da93e47dafbcdb
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-25 10:58:54 +0200

    Update

--- LillaStoldBilen/Assets/Scripts/Dollar.cs
+++ LillaStoldBilen/Assets/Scripts/Dollar.cs
@@ -1,8 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;using UnityEngine.Events;
 
 public class Dollar : MonoBehaviour
 {
+    public Player player;
     public int dollar = 50; //Value of dollar
+
+    public void OnTriggerEnter(Collider player)
+    {
+        
+    }
+
+    void Update() // Rotation animation
+    {
+        
+    }
 }
--- LillaStoldBilen/Assets/Scripts/Player.cs
+++ LillaStoldBilen/Assets/Scripts/Player.cs
@@ -8,8 +8,20 @@ public class Player : MonoBehaviour
 
 {
     public int Health = 90;
+    public bool isAlive = true;
     public float MovementSpeed;
     public int MoneyBag;
     public bool onHand;
     public GameObject InHand = new GameObject();
+
+    void Update()
+    {
+        CheckAlive();
+    }
+    
+    private void CheckAlive()
+    {
+        if (Health > 0)
+            isAlive = false;
+    }
 }
