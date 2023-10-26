16ec3b3e7f767cd3e7620dab2d4dfa5d829d22d0
RachelLach <144023790+RachelLach@users.noreply.github.com>
2023-10-26 09:52:34 +0200

    Apply changes to prefab

---
+++ LillaStoldBilen/Assets/Scripts/Orangecardamage.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orangecardamage : MonoBehaviour
+{
+    public float maxHealthForOrangeCar = 50f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
