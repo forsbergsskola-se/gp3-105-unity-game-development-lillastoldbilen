ae632dd176eaba50aa8eb7d0069222dbc992c905
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2023-10-30 11:27:41 +0100

    Remove demonstration buttons

--- LillaStoldBilen/Assets/Scripts/ButtonActions.cs
+++ LillaStoldBilen/Assets/Scripts/ButtonActions.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonActions : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
     public void DoExitGame() 
     {
 #if UNITY_EDITOR
