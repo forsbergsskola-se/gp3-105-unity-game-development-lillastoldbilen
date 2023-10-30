f5200d8150aef6a5bcda53d982077ee60d966ffb
reynin <angelo.panghulan@student.forsbergsskola.se>
2023-10-30 12:17:40 +0100

    Update Fire Prefab

--- LillaStoldBilen/Assets/Scripts/Fire_Damage.cs
+++ LillaStoldBilen/Assets/Scripts/Fire_Damage.cs
@@ -6,12 +6,13 @@ using UnityEngine.UIElements;
 
 public class Fire_Damage : MonoBehaviour
 {
-    public void OnCollisionStay(Collision other)
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.name == "Player")
         {
             PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
-            float takenDamage = 2 * Time.deltaTime;
+            float takenDamage = 5 * Time.deltaTime;
             player.TakeDamage(takenDamage);
         }
     }
